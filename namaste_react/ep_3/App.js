509aab65c163f1912ef2bfa4ee0ec18235e3f92f
DishaKathane <dishakathane1995@gmail.com>
2025-03-23 15:47:43 +0530

    function component

--- namaste_react/ep_3/App.js
+++ namaste_react/ep_3/App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from React🙏!"
-);
-console.log(heading);
-//JSX - HTML-like / XML-like but it's a javascript syntax
-const jsxHeading = <h1> Welcome to JSX world 🙏!</h1>
-console.log(jsxHeading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);  //reactdom takes the html and push it the browser
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello world from React🙏!"
+// );
+// console.log(heading);
+// //JSX - HTML-like / XML-like but it's a javascript syntax
+// const jsxHeading = <h1> Welcome to JSX world 🙏!</h1>
+// console.log(jsxHeading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);  //reactdom takes the html and push it the browser
 
 
 //JSX --> JS SYNTAX WHICH IS EASIER TO CREATE REACT ELEMENT (it is not the part of react) 
 // jsx makes developer life easy.
-//
+//REACT component  == write a javascript function and return JSX (react element) called react functional component
+/**
+ * CLASS COMPONENT - OLD Way
+ * FUNCTIONAL COMPONENT -NEW Way
+ */
+const Title = ()=>(
+  <h1 className="head" tabIndex="5">This is title functional component!</h1>
+)
+const Heading =()=> (
+  <div className="container">
+    <Title />
+<h1 className="heading"> Welcome to functional component world 🙏!</h1>
+  </div>
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>); 
